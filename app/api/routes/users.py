9a06641c104a0a22d77a623ed789aa98d7b8ece0
tthisag246 <tthisag246@gmail.com>
2025-06-01 03:26:10 +0900

    [feat] 회원정보 수정 POST api 구현

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Header, Depends
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
 from app.api.dependencies import get_user
+from app.core.database import get_db
 from app.models.user import User
-from app.schemas.user import UserScoreResponse
+from app.schemas.user import UserScoreResponse, UserExtraInfoRequest
 from app.services import user_service
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -15,3 +18,18 @@ def read_user_score(
         user: User = Depends(get_user)
 ):
     return user_service.get_user_score(user)
+
+@router.post(
+    ""
+)
+
+
+@router.post(
+    "/extra",
+)
+def create_user_extra_info(
+        user_request: UserExtraInfoRequest,
+        user: User = Depends(get_user),
+        db: Session = Depends(get_db)
+):
+    user_service.create_user_extra_info(user_request, db, user)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,13 @@
+from app.crud import user_crud
 from app.models.user import User
 from app.schemas.user import UserScoreResponse
 
 
 def get_user_score(user: User):
     return UserScoreResponse(score=user.user_score)
+
+
+def create_user_extra_info(user_request, db, user):
+    user = user_crud.create_user_extra_info(user_request, db, user)
+    db.commit()
+    return True
